41b40cc54710b3c264bb570a0b9dba12e38a3d89
nch <ignacio.figueroara@sansano.usm.cl>
2020-03-17 13:04:48 -0300

    Ver 0.4.1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,16 @@ class PostsController < ApplicationController
     redirect_to @section
   end
 
+  def destroy
+    @section = Section.find(params[:section_id])
+    @post = @section.posts.find(params[:id])
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to section_path(@section), notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
     def post_params
       params.require(:post).permit(:message, :user_id, :section_id, :image)
